1f9ebc12dcda64f3dbe0c28e6e68198f560fb304
Elaina <elainateramoto@gmail.com>
2018-07-10 15:07:58 -0700

    added authentication

--- Makestagram/New Group1/LoginViewController.swift
+++ Makestagram/New Group1/LoginViewController.swift
@@ -8,12 +8,21 @@
 
 import Foundation
 import UIKit
+import FirebaseAuth
+import FirebaseUI
 
+typealias FIRUser = FirebaseAuth.User
 class LoginViewController: UIViewController {
     
     @IBOutlet weak var loginButton: UIButton!
     
     @IBAction func loginButtonTapped(_ sender: UIButton) {
+        
+        guard let authUI = FUIAuth.defaultAuthUI()
+            else {return}
+        authUI.delegate = self
+        let authViewController = authUI.authViewController()
+        present(authViewController, animated: true)
         print("login button was tapped!!")
     }
     override func viewDidLoad() {
@@ -24,4 +33,13 @@ class LoginViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
+}
+extension LoginViewController: FUIAuthDelegate{
+    func authUI(_ authUI: FUIAuth, didSignInWith user: FIRUser?, error: Error?){
+        if let error = error{
+            assertionFailure("Error signing in: \(error.localizedDescription)")
+            return
+        }
+        print("handle user signup/login")
+    }
 }
